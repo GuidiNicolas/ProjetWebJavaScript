1b63efcd044120354f813627fd2d5e63971e1d7b
guidinicolas <guidinicolas@gmail.com>
2017-01-31 13:36:03 +0100

    debut angular

---
+++ app/controlleurs/actionsRecherche_Controller.js
@@ -0,0 +1,15 @@
+angular.module('action').controller('actionsRechercheController',
+    ['$scope', '$http', 'Product', function($scope, $http, Product) {
+
+        $http.get('http://localhost:9292/products')
+            .then(function(response) {
+                $scope.products = [];
+                response.data.products.forEach(function(data) {
+                    var newProduct = new Product(data);
+                    $scope.products.push(newProduct);
+                });
+            }, function(error) {
+                console.log(error);
+            });
+
+    }]);
---
+++ app/controlleurs/controlleur_Recherche.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Nicochu on 31/01/2017.
+ */
+
